aec98159c3b84a730d120bd667cd40f35ac8d0a3
Odile <41570723+Odile-nza@users.noreply.github.com>
2022-01-04 02:15:39 +0200

    Create main.py
    character segmentation

---
+++ main.py
@@ -0,0 +1,44 @@
+from PIL.Image import ImageTransformHandler
+import cv2
+import numpy as np
+import pytesseract
+import mysql.connector
+
+pytesseract.pytesseract.tesseract_cmd="C:/Program Files/Tesseract-OCR/tesseract.exe"
+
+cascade= cv2.CascadeClassifier("haarcascade.xml")
+def extract_num(img_filename):
+    img=cv2.imread(img_filename)
+    #Img To Gray
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    nplate=cascade.detectMultiScale(gray,1.1,4)
+    #crop portion
+    for (x,y,w,h) in nplate:
+        wT,hT,cT=img.shape
+        a,b=(int(0.02*wT),int(0.02*hT))
+        plate=img[y+a:y+h-a,x+b:x+w-b,:]
+        #make the img more darker to identify LPR
+        kernel=np.ones((1,1),np.uint8)
+        plate=cv2.dilate(plate,kernel,iterations=1)
+        plate=cv2.erode(plate,kernel,iterations=1)
+        plate_gray=cv2.cvtColor(plate,cv2.COLOR_BGR2GRAY)
+        (thresh,plate)=cv2.threshold(plate_gray,130,255,cv2.THRESH_BINARY)
+        #read the text on the plate
+        read=pytesseract.image_to_string(plate,config='--psm 11')
+        print(read)
+        read=''.join(e for e in read if e.isalnum())
+        stat=read[0:2]
+        print(stat)
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        cv2.rectangle(img,(x-10,y-50),(x+w+1,y),(255,0,0),-1)
+        cv2.putText(img,"Number Plate",(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,0.9,(255,255,255),2)
+        
+        cv2.imshow("plate",plate)
+        
+    cv2.imwrite("Result.png",img)
+    cv2.imshow("Result",img)
+    if cv2.waitKey(0)==113:
+        exit()
+    cv2.destroyAllWindows()
+
+extract_num("car2.jpg")
